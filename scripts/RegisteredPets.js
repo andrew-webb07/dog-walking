f0de1ba71cc0235c77b34c807bd91d47f2654839
Andrew Webb <webba@ihopu.org>
2021-04-26 13:22:34 -0500

    Added click event on pets to show who walks each pet

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,39 @@
-import { getPets } from "./database.js"
+import { getPets , getWalkers } from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
+
+document.addEventListener(
+    "click", (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if(itemClicked.id.startsWith("pet")) {
+            const [, petId] = itemClicked.id.split("--")
+
+            
+
+
+            for (const pet of pets) {
+                if(pet.id === parseInt(petId)) {
+                    
+                    const petWalker = walkers.find(walker => walker.id === pet.walkerId)
+
+                    window.alert(`${pet.name} is walked by ${petWalker.name}`)
+
+                //     const petWalker = walkers.filter(walker => pet.walkerId === walker.id)
+                
+                // window.alert(`${pet.name} is walked by ${petWalker[0].name}`)
+                // }
+            }
+        }
+    }
+}
+)
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
